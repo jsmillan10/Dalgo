7388ce9bdb0014ca1036b68cbe96905c360b9a37
naguilar12 <n.aguilar@uniandes.edu.co>
2017-05-28 19:46:34 -0500

    Problema B

--- DALGO/src/Problemas/ProblemaB.java
+++ DALGO/src/Problemas/ProblemaB.java
@@ -1,10 +1,57 @@
 package Problemas;
 
+import java.util.Scanner;
+
 public class ProblemaB {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
+		Scanner in = new Scanner(System.in);
+
+		while(true)
+		{
+			int nSocios = in.nextInt();
+			int[] porSoc = new int [nSocios];
+
+			for (int i = 0; i < porSoc.length; i++) {
+				porSoc[i] = in.nextInt();
+			}
+
+			boolean[] sePuede = new boolean [101];
+			double resp = 0;
+			
+			recurrencia(100,0, sePuede, porSoc);
+			
+			for(int i = 51; i <= 100; i++)
+				if(sePuede[i])
+				{
+					resp = (double) porSoc[porSoc.length-1]*100/i;
+					break;
+				}
+			
+			resp = (double) Math.round(resp*100)/100;
+			System.out.println(resp);
+		}
+	}
+
+	public static void recurrencia(int a, int j, boolean[] sePuede ,int[] porc)
+	{
+		if(j == 0)
+			sePuede[a] = (a==porc[porc.length-1]);
+		else if(a<porc[(j+porc.length-1)%porc.length])
+			sePuede[a] = sePuede[a];
+		else if(a>=porc[(j+porc.length-1)%porc.length])
+			sePuede[a] = sePuede[a] || sePuede[a-porc[(j+porc.length-1)%porc.length]];
+
+		if(a==0)
+		{
+			j++;
+			a=100;
+		}
+		else
+			a--;
+		if(j<porc.length)
+			recurrencia(a, j, sePuede, porc);
 	}
 
 }
